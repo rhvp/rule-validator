0bd312e5c3bb747c2eaaeb59f4d4908730d68706
Abdul Ajoge <bugsabdul@yahoo.com>
2021-01-30 00:12:55 +0100

    add heroku congig

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ process.on('uncaughtException', err => {
     console.log('Uncaught Exception!! Shutting down process..', err.name, err.message, err.stack);
     process.exit(1);
 });
-app.listen(1080, ()=>{
-    console.log('app running on: 1080...');
+let port = 1080 || process.env.PORT;
+app.listen(port, ()=>{
+    console.log('app running on: ' + port);
 })
 process.on('unhandledRejection', err=>{
     console.log('Unhandled Rejection!!',err.code, err.name, err.message, err.stack);
