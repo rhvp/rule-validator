ffe170ebfde65199c4b4315744b760bf48a1ea30
Abdul Ajoge <bugsabdul@yahoo.com>
2021-01-30 00:22:57 +0100

    add heroku congig

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const app = require('./app');
 
 Error.stackTraceLimit = 3;
@@ -6,7 +5,7 @@ process.on('uncaughtException', err => {
     console.log('Uncaught Exception!! Shutting down process..', err.name, err.message, err.stack);
     process.exit(1);
 });
-let port = 1080 || process.env.PORT;
+let port = process.env.PORT || 1080;
 app.listen(port, ()=>{
     console.log('app running on: ' + port);
 })
